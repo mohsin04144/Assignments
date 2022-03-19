be3b0f612972fae62df7dced205697c1de5a25c6
SHAIK MOHSIN S <98951493+mohsin04144@users.noreply.github.com>
2022-03-19 12:13:54 +0530

    added

---
+++ Spring MVC/springmvc6/src/main/java/springmvc6/TestController.java
@@ -0,0 +1,31 @@
+package springmvc6;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/registration.html")
+    public ModelAndView registrationForm() {
+        ModelAndView modelAndView = new ModelAndView("registration");
+        return modelAndView;
+
+    }
+    @RequestMapping("/validation.html")
+    public ModelAndView formValidation(@Valid @ModelAttribute("customer")Customer customer1, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            System.out.println("inside");
+            ModelAndView modelAndView = new ModelAndView("registration");
+            return modelAndView;
+        }
+        ModelAndView modelAndView = new ModelAndView("test");
+        modelAndView.addObject("msg",customer1);
+        return modelAndView;
+    }
+}
